0e205f0234283a5c8bf7948bb080a0b2565ad53d
Pradeep <chaudharipradeep9@gmail.com>
2013-09-29 22:19:10 -0700

    calculating for first observation

--- hmm.py
+++ hmm.py
@@ -5,23 +5,33 @@ import sys, hmmtrain
 
 class hmm:        
     def __init__(self):
-		'''
         priors, transitions, emissions, states, symbols = hmmtrain.train()
         self.priors = priors
         self.transitions = transitions
         self.emissions = emissions
         self.states = states
         self.symbols = symbols
-		'''
+		
 		
     def exhaustive(self):
 		input = 'You look around at professional ballplayers and nobody blinks an eye'
 		input = input.lower()    #convert to lower case
 		input = input.split()   #tokenize
-		viterbi= zeros(shape=(12,len(input)),dtype=float32);  #float32 numpy array for precision,  hardcoded lenth of token for alog, modify later size+2
+		viterbi= zeros(shape=(len(self.states)+2,len(input)),dtype=float32);  #float32 numpy array for precision
+		backpointers= zeros(shape=(len(self.states)+2,len(input)));
 		print viterbi
 		print input
-		
+		sym=input[0]  #take first observation/word
+		i=0
+		for state in self.states:
+			viterbi[i,0]= self.priors.logprob(state) + self.emissions[state].logprob(sym) #use priors for start frequency, log scale therfore plus
+			backpointers[i,0]=0
+			i=i+1
+			#save backpoint
+			#print state
+		print viterbi
+		print 'backpointers'
+		print backpointers
 		
     ####
     # ADD METHODS HERE
