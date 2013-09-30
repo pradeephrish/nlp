fd92183bed30efd76f7b0137a4486ea48e18ea78
Pradeep <chaudharipradeep9@gmail.com>
2013-09-30 01:50:54 -0700

    used enumerate function for looping, removed local index variables

--- hmm.py
+++ hmm.py
@@ -14,10 +14,10 @@ class hmm:
 	
 		
     def exhaustive(self):
-		input = 'You look around at professional ballplayers and nobody blinks an eye'
+		input = 'You look around at professional ballplayers and nobody blinks an eye'+ ' .'
 		input = input.lower()    #convert to lower case
 		input = input.split()   #tokenize
-		input = input.append('.') #for termination phase
+		#input = input.append('.') #for termination phase    -- added with string
 		viterbi= zeros(shape=(len(self.states)+2,len(input)),dtype=float32);  #float32 numpy array for precision
 		backpointers= zeros(shape=(len(self.states)+2,len(input)));
 		print viterbi
@@ -34,31 +34,24 @@ class hmm:
 		print 'backpointers'
 		print backpointers
 		iterinputs = iter(input)
-		next(iterinputs)      #skip first element
-		k=1
-		for word in iterinputs:
-			i = 0
-			for state in self.states:
+		for k,word in enumerate(iterinputs):
+			for s,state in enumerate(self.states):
 				#find maximum from viterbi[s',t-1] and store that s' for a(s',s)
 				max=viterbi[0,k-1];
 				argMaxSum=viterbi[0,k-1]+ self.transitions[state].logprob(state)
 				maxStateId = 0; #since max initialized to zero, this is s'
 				argMaxStateId=0;
-				p = 0
-				for stateIn in self.states
-					if max < viterbi[p,k-1]
+				for p,stateIn in enumerate(self.states):
+					if max < viterbi[p,k-1]:
 						max=viterbi[p,k-1]
 						maxStateId = p;
-					if argMaxSum < viterbi[p,k-1]+ self.transitions[stateIn].logprob(state)
+					if argMaxSum < viterbi[p,k-1]+ self.transitions[stateIn].logprob(state):
 						argMaxSum= viterbi[p,k-1]+ self.transitions[stateIn].logprob(state)
 						argMaxStateId=p;
-					p=p+1
 						
 				#above function finished, found maximum ,s' stored in maxStateId   ,  Note  viterbi[maxStateId,k-1] is equal to max
-				viterbi[i,k]=viterbi[maxStateId,k-1]+ self.transitions[self.states[maxStateId]].logprob(state)+self.emissions[state].logprob(word)#note log scale -> plus 
-				backpointers[i,k]=	argMaxStateId
-		        i = i+1
-		k = k+1
+				viterbi[s,k]=viterbi[maxStateId,k-1]+ self.transitions[self.states[maxStateId]].logprob(state)+self.emissions[state].logprob(word)#note log scale -> plus
+				backpointers[s,k]=argMaxStateId
 		#termination step  T (oT = '.') , input/words/observations were appended by . therefore termination should happen at this stage
 		'''
 		print 'before final step'
@@ -70,14 +63,17 @@ class hmm:
     ####
     # ADD METHODS HERE
     ####
+		print 'terminating'
+		print viterbi
+		print backpointers
 		return backpointers
 
 
 def main():
     # Create an instance
     model = hmm().exhaustive()
-	print 'everthing went fine, now printing backpointers'
-	print model
+	#print 'everthing went fine, now printing backpointers'
+	#print model
 
 	
 	
