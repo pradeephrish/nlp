48af46a2b20404394dd716df443bd591bdcbed04
Pradeep <chaudharipradeep9@gmail.com>
2013-09-30 00:07:12 -0700

    viterbi in progress t Please enter the commit message for your changes. Lines starting

--- hmm.py
+++ hmm.py
@@ -11,7 +11,7 @@ class hmm:
         self.emissions = emissions
         self.states = states
         self.symbols = symbols
-		
+	
 		
     def exhaustive(self):
 		input = 'You look around at professional ballplayers and nobody blinks an eye'
@@ -34,10 +34,30 @@ class hmm:
 		print backpointers
 		iterinputs = iter(input)
 		next(iterinputs)      #skip first element
+		k=1
 		for word in iterinputs:
-			
-		
-		
+			i = 0
+			for state in self.states:
+				#find maximum from viterbi[s',t-1] and store that s' for a(s',s)
+				max=viterbi[0,k-1];
+				argMaxSum=viterbi[0,k-1]+ self.transitions[state].logprob(state)
+				maxStateId = 0; #since max initialized to zero, this is s'
+				argMaxStateId=0;
+				p = 0
+				for stateIn in self.states
+					if max < viterbi[p,k-1]
+						max=viterbi[p,k-1]
+						maxStateId = p;
+					if argMaxSum < viterbi[p,k-1]+ self.transitions[stateIn].logprob(state)
+						argMaxSum= viterbi[p,k-1]+ self.transitions[stateIn].logprob(state)
+						argMaxStateId=p;
+					p=p+1
+						
+				#above function finished, found maximum ,s' stored in maxStateId   ,  Note  viterbi[maxStateId,k-1] is equal to max
+				viterbi[i,k]=viterbi[maxStateId,k-1]+ self.transitions[self.states[maxStateId]].logprob(state)+self.emissions[state].logprob(word)#note log scale -> plus 
+				backpointers[i,k]=
+		        i = i+1
+		k = k+1
     ####
     # ADD METHODS HERE
     ####
